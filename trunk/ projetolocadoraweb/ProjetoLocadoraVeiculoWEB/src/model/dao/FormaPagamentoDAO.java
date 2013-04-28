bdc3669659ea8512f99bcb7092f8bced813587d3
dto.diego@gmail.com <dto.diego@gmail.com@81e5a777-ab14-9313-4a0b-7c217bf2d61b>
2013-04-28 20:08:33 +0000

    Commit Driver Postgres model.dao

---
+++ trunk/ projetolocadoraweb/ProjetoLocadoraVeiculoWEB/src/model/dao/FormaPagamentoDAO.java
@@ -0,0 +1,72 @@
+//Classe FormaPagamentoDAO
+package model.dao;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import model.Cheque;
+
+public class FormaPagamentoDAO extends FormaPagamentoGenericoDAO{
+
+	
+	public FormaPagamentoDAO(){
+		super();
+	}
+	
+	
+	public void inserirFormaPag(Cheque cheque){
+		super.inserirFormaPag(cheque);
+		int ultimoRegistro = obterUltimoRegistro();
+		
+		String sqlInsert = "insert into cheque (nomeBanco , numAgencia , numCCO , nomeTitular , cpfTitular , foneTitular , idFormaPagamentoFK) values " +
+							"(? , ? , ? , ? , ? , ? , ?)";
+		
+		PreparedStatement stm = null;
+		
+		try 
+		{
+			//
+			// Inclusao dos dados na tabela Cheque
+			//
+			stm = conn.prepareStatement(sqlInsert);
+
+			stm.setString(1, cheque.getNomeBanco());
+			stm.setString(2, cheque.getNumAgencia());
+			stm.setString(3, cheque.getNumCCO());
+			stm.setString(4, cheque.getNomeTitular());
+			stm.setString(5, cheque.getCpfTitular());
+			stm.setString(6, cheque.getFoneTitular());
+			stm.setInt(7, ultimoRegistro);
+			stm.execute();
+			
+			conn.commit();
+
+		}
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+				try 
+				{
+					conn.rollback();
+				}
+				catch (SQLException e1) 
+				{
+					System.out.print(e1.getStackTrace());
+				}
+		} 
+		finally 
+		{
+			if (stm != null) 
+			{
+				try 
+				{
+					stm.close();
+				} 
+				catch (SQLException e1) 
+				{
+					System.out.print(e1.getStackTrace());
+				}
+			}
+		}
+	}	
+}
