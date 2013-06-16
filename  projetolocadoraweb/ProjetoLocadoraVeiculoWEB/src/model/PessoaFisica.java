e178f38fbce65695bfeea4d4edd01b5e5bff621e
rickcatelani <rickcatelani@gmail.com>
2013-06-16 13:50:22 +0000

    Ola tudo bem =)

---  projetolocadoraweb/ProjetoLocadoraVeiculoWEB/src/model/PessoaFisica.java
+++  projetolocadoraweb/ProjetoLocadoraVeiculoWEB/src/model/PessoaFisica.java
@@ -1,7 +1,12 @@
 //Classe PessoaFisica
 package model;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
+import model.dao.ClienteDAO;
 
 public class PessoaFisica extends Cliente{
 	
@@ -14,6 +19,7 @@ public class PessoaFisica extends Cliente{
 	private Date primeiraHabilitacao;
 	private Date validadeCartMotorista;
 	
+	//Construtor sem parametros, inicializa os metodos em branco
 	public PessoaFisica(){
 		super();
 		setDtaNascimento(null);
@@ -25,10 +31,9 @@ public class PessoaFisica extends Cliente{
 		setValidadeCartMotorista(null);
 	}
 	
-	
-	
-	public PessoaFisica(String nome , String registro , String fone, String email, Date dtaNascimento, String sexo, String rg,
-			String numHabilitacao, String estadoEmissor, Date primeiraHabilitacao , Date validadeCarteira) {
+	//Construtor com parametros
+	public PessoaFisica(String nome , String registro , String fone, String email, Date dtaNascimento, String sexo,
+					String rg,String numHabilitacao, String estadoEmissor, Date primeiraHabilitacao , Date validadeCarteira) {
 		
 		super(nome , registro , fone , email);
 		setDtaNascimento(dtaNascimento);
@@ -41,54 +46,73 @@ public class PessoaFisica extends Cliente{
 		
 	}
 
-
-
+//Getters and Setters==================================================================================================
+	
+	//GET DATA NASCIMENTO
 	public Date getDtaNascimento() {
 		return dtaNascimento;
 	}
+	//SET DATA DE NASCIMENTO
 	public void setDtaNascimento(Date dtaNascimento) {
 		this.dtaNascimento = dtaNascimento;
 	}
+	// GET SEXO
 	public String getSexo() {
 		return sexo;
 	}
+	// SET SEXO
 	public void setSexo(String sexo) {
 		this.sexo = sexo;
 	}
-
+	//GET RG
 	public String getRg() {
 		return rg;
 	}
+	//SET RG
 	public void setRg(String rg) {
 		this.rg = rg;
 	}
+	//GET NUMERO DA HABILITACAO 
 	public String getNumHabilitacao() {
 		return numHabilitacao;
 	}
+	//SET NUMERO DA HABILITACAO
 	public void setNumHabilitacao(String numHabilitacao) {
 		this.numHabilitacao = numHabilitacao;
 	}
+	//GET ESTADO EMISSOR
 	public String getEstadoEmissor() {
 		return estadoEmissor;
 	}
+	//SET ESTADO EMISSOR
 	public void setEstadoEmissor(String estadoEmissor) {
 		this.estadoEmissor = estadoEmissor;
 	}
+	//GET VALIDADE DE CARTEIRA DE MOTORISTA
 	public Date getValidadeCartMotorista() {
 		return validadeCartMotorista;
 	}
+	//SET VAIDADE DA CARTEIRA DE MOTORISTA
 	public void setValidadeCartMotorista(Date validadeCartMotorista) {
 		this.validadeCartMotorista = validadeCartMotorista;
 	}
-
+	//SET PRIMEIRA HABILITACAO
 	public Date getPrimeiraHabilitacao() {
 		return primeiraHabilitacao;
 	}
-
+	//GET PRIMEIRA HABILITACAO
 	public void setPrimeiraHabilitacao(Date primeiraHabilitacao) {
 		this.primeiraHabilitacao = primeiraHabilitacao;
 	}
-	
+	//GET IDPF	
+	public int getIdPF() {
+		return idPF;
+	}
+	//SET IDPF
+	public void setIdPF(int idPF) {
+		this.idPF = idPF;
+	}
+//=====================================================================================================================
 	public String toString(){
 		
 		return getId() + "\n" +
@@ -102,17 +126,26 @@ public class PessoaFisica extends Cliente{
 			getPrimeiraHabilitacao() + "\n" +
 			getValidadeCartMotorista();
 	}
-
-
-
-	public int getIdPF() {
-		return idPF;
+	
+	//Metodo responsavel por incluir um novo cliente PF
+	public void incluir(){
+		ClienteDAO cDAO = new ClienteDAO();
+		cDAO.cadastrar(this);
 	}
-
-
-
-	public void setIdPF(int idPF) {
-		this.idPF = idPF;
+	
+	//Metodo responsavel por alterar um cliente existente no banco de dados
+	public void alterar(PessoaFisica pf){
+		ClienteDAO cDAO = new ClienteDAO();
+		cDAO.alterar(this);	
+	}
+	//Metodo responsavel por exibir os dados de um cliente
+	public PessoaFisica pesquisar(){
+		ClienteDAO cDAO = new ClienteDAO();
+		return cDAO.consultarPF(getRegistro());
+	}
+	//Metodo responsave por excluir um cliente da base de dados
+	public void excluir(){
+		
 	}
 
 }
