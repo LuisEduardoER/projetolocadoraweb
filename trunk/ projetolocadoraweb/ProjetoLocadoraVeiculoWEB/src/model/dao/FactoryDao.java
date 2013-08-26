9bbafc32c885d20d25f89ded09af5039d344e065
dto.diego@gmail.com <dto.diego@gmail.com@81e5a777-ab14-9313-4a0b-7c217bf2d61b>
2013-08-26 12:07:24 +0000

    Factory Diego

--- trunk/ projetolocadoraweb/ProjetoLocadoraVeiculoWEB/src/model/dao/FactoryDao.java
+++ trunk/ projetolocadoraweb/ProjetoLocadoraVeiculoWEB/src/model/dao/FactoryDao.java
@@ -1,7 +1,12 @@
 package model.dao;
 
+
 public class FactoryDao {
+	
 	public static VeiculoDao obterVeiculoDao() {
+		System.out.println("teste factory");
 		return new VeiculoPostgresDao();
 	}
+	
 }
+
