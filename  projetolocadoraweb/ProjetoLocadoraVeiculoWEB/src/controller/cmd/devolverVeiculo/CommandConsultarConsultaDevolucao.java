fb7a2c6e41ce349cb24ef0274bdb1184a89fcbad
dto.diego <dto.diego@gmail.com>
2013-10-16 16:35:55 +0000

    Consultar Locacao

---  projetolocadoraweb/ProjetoLocadoraVeiculoWEB/src/controller/cmd/devolverVeiculo/CommandConsultarConsultaDevolucao.java
+++  projetolocadoraweb/ProjetoLocadoraVeiculoWEB/src/controller/cmd/devolverVeiculo/CommandConsultarConsultaDevolucao.java
@@ -1,8 +1,9 @@
 package controller.cmd.devolverVeiculo;
 
+import java.util.ArrayList;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import model.Locacao;
 import controller.cmd.Command;
 
 public class CommandConsultarConsultaDevolucao extends Command{
@@ -11,44 +12,61 @@ public class CommandConsultarConsultaDevolucao extends Command{
 	public String executar(HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
 		
-		return null;
+//		LocacaoDAO dao = new LocacaoDAO();
+		String nextPage = null;
+		Locacao locacao = new Locacao();
+		request.getSession().setAttribute("listaIDsLocacao", 0);
+		int idLocacao = (Integer) request.getAttribute("txtIdLocacaoConsulta");
+		String szCnpjCpf = (String) request.getAttribute("txtCnpjCpfConsulta");
+		
+		
+		try{
+			if(idLocacao > 0){
+				if(locacao.isDevolvido(idLocacao)){
+//					Mensagem.impossivelEncontrarLocacaoFinalizada();
+					System.out.println( "Não foi possível encontrar locação pois a mesma está finalizada");
+					nextPage = "DevolucaoConsulta.jsp";
+				}else{
+					locacao = locacao.consultar(idLocacao);
+					if(locacao == null){
+//						Mensagem.impossivelEncontrarLocacao();
+						System.out.println( "Não foi possível encontrar locação");
+						nextPage = "DevolucaoConsulta.jsp";
+					}else{
+//						calculaQtdDias();
+//						carregarValoresCampos();
+						//ENCAMINHAR PARA A PRÓXIMA TELA
+						nextPage = "DevolucaoValoresAdicionais.jsp";
+					}
+				}
+			}
+		}catch(NumberFormatException e){
+			if(szCnpjCpf != ""){
+				ArrayList<Integer> nroLocacoes = locacao.consultaLocacao(szCnpjCpf);
+				
+				if(nroLocacoes.isEmpty()){ //caso não for possível encontrar locação 
+//					Mensagem.impossivelEncontrarLocacaoCPF();
+					System.out.println( "Não foi possível encontrar Locação com o CPF informado");
+					nextPage = "DevolucaoConsulta.jsp";
+				}else{
+					int i = 0;
+					String resultado = "";
+					while(i < nroLocacoes.size()){
+						resultado = resultado + "ID: " + nroLocacoes.get(i) + "\n";
+						i++;
+					}
+//					JOptionPane.showMessageDialog(null, resultado); //mostra todos os IDs das locações do cliente
+					request.getSession().setAttribute("listaIDsLocacao", resultado);
+				}
+			}else{
+				nextPage = "DevolucaoConsulta.jsp";
+			}
+		}
+//		consultaView.limparCamposSuperior();
+		
+		return nextPage;
 	}
 	
-	
-	
-	
-//	LocacaoDAO dao = new LocacaoDAO();
-//	try{
-//		if(consultaView.getTxtIdLocacaoConsulta() != 0){
-//			if(dao.isDevolvido(consultaView.getTxtIdLocacaoConsulta())){
-//				Mensagem.impossivelEncontrarLocacaoFinalizada();
-//			}else{
-//				locacao = dao.consultar(consultaView.getTxtIdLocacaoConsulta());
-//				if(locacao == null){
-//					Mensagem.impossivelEncontrarLocacao();
-//				}else{
-//					calculaQtdDias();
-//					carregarValoresCampos();
-//				}
-//			}
-//		}
-//	}catch(NumberFormatException e){
-//		if(consultaView.getTxtCnpjConsulta() != ""){
-//			ArrayList<Integer> nroLocacoes = dao.consultaLocacao(consultaView.getTxtCnpjConsulta());
-//			if(nroLocacoes.isEmpty()){ //caso não for possível encontrar locação 
-//				Mensagem.impossivelEncontrarLocacaoCPF();
-//			}else{
-//				int i = 0;
-//				String resultado = "";
-//				while(i < nroLocacoes.size()){
-//					resultado = resultado + "ID: " + nroLocacoes.get(i) + "\n";
-//					i++;
-//				}
-//				JOptionPane.showMessageDialog(null, resultado); //mostra todos os IDs das locações do cliente
-//			}
-//		}
-//	}
-//	consultaView.limparCamposSuperior();
 	
 
 }
